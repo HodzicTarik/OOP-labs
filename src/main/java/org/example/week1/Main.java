e7cc7064715fca5f469e777c27c35307733a5f7f
HodzicTarik <tarik.hodzic@stu.ibu.edu.ba>
2024-10-09 16:34:39 +0200

    Week 1

---
+++ src/main/java/org/example/week1/Main.java
@@ -0,0 +1,58 @@
+package org.example.week1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*// Task 1
+        System.out.print("My name is Tarik Hodzic");
+
+        // Task 2
+        System.out.println("Hello World");
+        System.out.print("");
+        System.out.print("And all the people of the world");
+
+        // Task 3
+        System.out.println("Enter the number of years: ");
+        Scanner year = new Scanner(System.in);
+        double years = year.nextDouble() * 365;
+        double secondsInYears = 60 * 60 * 24 * years;
+        System.out.print("Result: " + secondsInYears);
+
+    }
+
+    // Task 4
+    Scanner number = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+    double num1 = number.nextDouble();
+        System.out.println("Enter second number: ");
+    double num2 = number.nextDouble();
+        System.out.println("Sum of these 2 numbers is: " + (num1 + num2));
+
+    // Task 5
+    Scanner number = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+    double no = number.nextDouble();
+        if (no > 0){
+        System.out.println(no + " is a positive number");
+    } else if (no < 0) {
+        System.out.println(no + " is a negative number");
+    } else{
+        System.out.println(no + " is 0");
+    }
+
+    // Task 6
+    Scanner number = new Scanner(System.in);
+        System.out.println("Enter a first number: ");
+    int no1 = number.nextInt();
+        System.out.println("Enter a second number: ");
+    int no2 = number.nextInt();
+        if (no1 > no2){
+        System.out.println(no1 + " is greater than " + no2);
+    } else if (no1 < no2){
+        System.out.println(no1 + " is less than " + no2);
+    } else{
+        System.out.println("They are equal");
+    }*/
+
+    }
+}
